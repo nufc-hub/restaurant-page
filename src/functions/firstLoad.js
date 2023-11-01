822cda49c241430465f87bf7b2d06f787ff94018
nufc-hub <kmchenouf@gmail.com>
2023-11-01 20:52:01 +0900

    Add firstLoad function

---
+++ src/functions/firstLoad.js
@@ -0,0 +1,9 @@
+import createHeader from '../pages/header';
+import addHomeContent from '../pages/home';
+
+const firstLoad = () => {
+    createHeader();
+    addHomeContent();
+}
+
+export default firstLoad;
