b9b5b2c864e64f5ceab904f6ec943059f162f585
nufc-hub <kmchenouf@gmail.com>
2023-11-02 23:18:55 +0900

    Encapsulate menu items into divs

--- src/pages/menu.js
+++ src/pages/menu.js
@@ -2,49 +2,105 @@
 grated cabbage
 special - purple cabbage*/
 
-import firstImage from '../media/cabbage-juice.png'
+import firstImage from '../media/cabbage-juice.png';
+import secondImage from '../media/Sauerkraut-min.png';
+import thirdImage from '../media/fresh-purple-cabbage.png';
 
 function addMenuContent() {
 
-    const contentDiv = document.querySelector('.content');
- 
-    // Main Div parent container - child of contentDiv
- 
-    const mainDiv = document.createElement('div');
-    mainDiv.setAttribute('id', 'main-menu');
-    contentDiv.appendChild(mainDiv);
- 
-    // mainDiv children - child of mainDiv
- 
+  const contentDiv = document.querySelector('.content');
 
-    // Menu item one picture
-    const imgOne = document.createElement('img');
-    imgOne.setAttribute('id', 'img-one-menu');
-    imgOne.src = firstImage;
-    imgOne.alt = 'Blended cabbage juice in a glass';
-    mainDiv.appendChild(imgOne);
- 
-    // Menu item one description & price
- 
-    const imgOneText = document.createElement('div');
-    imgOneText.setAttribute('id', 'txt-one-menu');
+  // Main Div parent container - child of contentDiv
+
+  const mainDiv = document.createElement('div');
+  mainDiv.setAttribute('id', 'main-menu');
+  contentDiv.appendChild(mainDiv);
+
+  // mainDiv children - child of mainDiv
+
+  const itemOne = document.createElement('div');
+  const itemTwo = document.createElement('div');
+  const itemThree = document.createElement('div');
+
+  itemOne.setAttribute('id', 'item-one-menu');
+  itemTwo.setAttribute('id', 'item-two-menu');
+  itemThree.setAttribute('id', 'item-three-menu');
+
+  mainDiv.appendChild(itemOne);
+  mainDiv.appendChild(itemTwo);
+  mainDiv.appendChild(itemThree);
+
+  // Menu item picture
+
+  const imgOne = document.createElement('img');
+  const imgTwo = document.createElement('img');
+  const imgThree = document.createElement('img');
+
+  imgOne.setAttribute('id', 'img-one-menu');
+  imgTwo.setAttribute('id', 'img-two-menu');
+  imgThree.setAttribute('id', 'img-three-menu');
+
+  imgOne.src = firstImage;
+  imgTwo.src = secondImage;
+  imgThree.src = thirdImage;
+
+  imgOne.alt = 'Blended cabbage juice in a glass';
+  imgTwo.alt = 'Sauerkraut in a jar.';
+  imgThree.alt = 'A purple cabbage head.';
+
+  itemOne.appendChild(imgOne);
+  itemTwo.appendChild(imgTwo);
+  itemThree.appendChild(imgThree);
+
+  // Menu item description & price
+
+  const imgOneText = document.createElement('div');
+  const imgTwoText = document.createElement('div');
+  const imgThreeText = document.createElement('div');
+
+  imgOneText.setAttribute('id', 'txt-one-menu');
+  imgTwoText.setAttribute('id', 'txt-two-menu');
+  imgThreeText.setAttribute('id', 'txt-three-menu');
+  
+  const textNodeOne = document.createTextNode('Blended Cabbage');
+  const textNodeTwo = document.createTextNode('Sauerkraut');
+  const textNodeThree = document.createTextNode('Red Cabbage');
+  
+  
+  const spanElementOne = document.createElement('span');
+  const spanElementTwo = document.createElement('span');
+  const spanElementThree = document.createElement('span');
+
+  spanElementOne.textContent = '$1';
+  spanElementTwo.textContent = '$2';
+  spanElementThree.textContent = '$3';
+  
+  imgOneText.appendChild(textNodeOne);
+  imgTwoText.appendChild(textNodeTwo);
+  imgThreeText.appendChild(textNodeThree);
+
+  imgOneText.appendChild(spanElementOne);
+  imgTwoText.appendChild(spanElementTwo);
+  imgThreeText.appendChild(spanElementThree);
+  
+  itemOne.appendChild(imgOneText); 
+  itemTwo.appendChild(imgTwoText);
+  itemThree.appendChild(imgThreeText);
+
+
     
-    const textNodeOne = document.createTextNode('Blended Cabbage');
-    const textNodeTwo = document.createTextNode('.....');
+    
+    
+    
    
     
-    const spanElement = document.createElement('span');
-    spanElement.textContent = '$1';
-    
-    imgOneText.appendChild(textNodeOne);
-    imgOneText.appendChild(textNodeTwo);
-    imgOneText.appendChild(spanElement);
-    
-    mainDiv.appendChild(imgOneText);
 
-    // add item two container here to include all the elements created below.
-    // or think of ways to make this code shorter will still including all three menu items
+
+    
+
+
+    
  
-  }
+}
 
   export default addMenuContent;
