5d15b0fb07f950e5b28b638f0bb595c17167a8c6
nufc-hub <kmchenouf@gmail.com>
2023-11-05 15:23:01 +0900

    Refactor code

--- src/pages/home.js
+++ src/pages/home.js
@@ -2,46 +2,52 @@ import mainImage from "../media/pngimg.com - cabbage_PNG8785-min-resize2.png";
 
 function addHomeContent() {
 
-   const contentDiv = document.querySelector('.content');
+const contentDiv = document.querySelector('.content');
 
-   // Main Div parent container - child of contentDiv
+// Main Div parent container - child of contentDiv
 
-   const mainDiv = document.createElement('div');
-   mainDiv.setAttribute('id', 'main-home');
-   contentDiv.appendChild(mainDiv);
+const mainDiv = document.createElement('div');
 
-   // mainDiv children - child of mainDiv
+mainDiv.setAttribute('id', 'main-home');
 
-   const mainImg = document.createElement('img');
-   mainImg.setAttribute('id', 'main-home-img');
-   mainImg.src = mainImage;
-   mainImg.alt = 'An uncooked green cabbage head';
-   const sloganContainer = document.createElement('div');
-   sloganContainer.setAttribute('id', 'slogan-container-home');
-   mainDiv.appendChild(mainImg);
-   mainDiv.appendChild(sloganContainer);
+contentDiv.appendChild(mainDiv);
 
-   // Slogan - child of sloganContainer
+// mainDiv children - child of mainDiv
 
-   const sloganDiv = document.createElement('div');
-   sloganDiv.setAttribute('id', 'slogan-home');
-   
-   const textNodeOne = document.createTextNode('100% CABBAGE,');
-   const textNodeTwo = document.createTextNode('...');
-   const textNodeThree = document.createTextNode('...DAY');
-   
-   const lineBreak = document.createElement('br')
-   
-   const spanElement = document.createElement('span');
-   spanElement.textContent = 'EVERY';
-   
-   sloganDiv.appendChild(textNodeOne);
-   sloganDiv.appendChild(lineBreak);
-   sloganDiv.appendChild(textNodeTwo);
-   sloganDiv.appendChild(spanElement);
-   sloganDiv.appendChild(textNodeThree);
-   
-   sloganContainer.appendChild(sloganDiv);
+const mainImg = document.createElement('img');
+const sloganContainer = document.createElement('div');
+
+mainImg.setAttribute('id', 'main-home-img');
+sloganContainer.setAttribute('id', 'slogan-container-home');
+
+mainImg.src = mainImage;
+
+mainImg.alt = 'An uncooked green cabbage head';
+
+mainDiv.appendChild(mainImg);
+mainDiv.appendChild(sloganContainer);
+
+// Slogan - child of sloganContainer
+
+const sloganDiv = document.createElement('div');
+const lineBreak = document.createElement('br');
+const spanElement = document.createElement('span');
+
+sloganDiv.setAttribute('id', 'slogan-home');
+
+spanElement.textContent = 'EVERY';
+
+const textNodeOne = document.createTextNode('100% CABBAGE,');
+const textNodeTwo = document.createTextNode('...');
+const textNodeThree = document.createTextNode('...DAY');
+
+sloganDiv.appendChild(textNodeOne);
+sloganDiv.appendChild(lineBreak);
+sloganDiv.appendChild(textNodeTwo);
+sloganDiv.appendChild(spanElement);
+sloganDiv.appendChild(textNodeThree);
+
+sloganContainer.appendChild(sloganDiv);
  }
 
 export default addHomeContent;
