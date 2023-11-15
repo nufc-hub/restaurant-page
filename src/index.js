8bf956b27a50a3dadf3756cbd9f28acdd05636c1
nufc-hub <kmchenouf@gmail.com>
2023-11-15 20:55:18 +0900

    Refactor event listeners

--- src/index.js
+++ src/index.js
@@ -1,117 +1,36 @@
-const contentDiv = document.querySelector('.content');
+import '../src/styles/header.css';
+import '../src/styles/home.css';
+import '../src/styles/menu.css';
+import '../src/styles/contact.css';
+import addHomeContent from './pages/home';
+import addMenuContent from './pages/menu';
+import addContactContent from './pages/contact';
+import firstLoad from '../src/functions/firstLoad';
+import removeMainDiv from '../src/functions/removePageContent';
 
-initializePage();
+firstLoad(console.log('load'));
 
-function createHeader() {
-   const headerNavContainer = document.createElement('div');
-   headerNavContainer.classList.add('header-nav-container');
-   contentDiv.appendChild(headerNavContainer);
+const homeButton = document.getElementById('home');
+const menuButton = document.getElementById('menu');
+const contactButton = document.getElementById('contact');
 
-
-   const header = document.createElement('header');
-   header.classList.add('logo');
-   headerNavContainer.appendChild(header);
-
-
-   const headerText = document.createElement('h3');
-   const headerImg = document.createElement('img');
-   headerText.textContent = 'Dark Age Diet';
-   headerImg.src = '../src/media/5087623.png';
-   headerImg.alt = 'An icon of a ruin';
-   header.appendChild(headerText);
-   header.appendChild(headerImg);
-
-   return {
-      header,
-      headerNavContainer
-   };
-}
-
-function createNav(headerNavContainer) {
-   const nav = document.createElement('nav');
-   headerNavContainer.appendChild(nav);
-   
-   
-   const tabs = document.createElement('ul');
-   nav.appendChild(tabs);
-   
-   for (let i = 1; i <= 3; i++) {
-      
-      const li = document.createElement('li');
-      const a = document.createElement('a');
-   
-      if (i === 1) {
-         a.textContent = 'Home';
-         a.href = ''
-      } else if (i === 2) {
-         a.textContent = 'Menu';
-         a.href = ''
-      } else if (i === 3) {
-         a.textContent = 'Contact';
-         a.href = ''
-      }
-   
-      tabs.appendChild(li);
-      li.appendChild(a);
-   }
-
-   return nav;
-}
-
-function createMainDiv() {
-   const mainDiv = document.createElement('div');
-   mainDiv.classList.add('main');
-   contentDiv.appendChild(mainDiv);
-
-   return mainDiv;
-}
+homeButton.addEventListener('click', () => {
+    removeMainDiv();
+    addHomeContent();
+    console.log('home');
+})
 
 
-function createMainDivElements(mainDiv) {
-   const mainImg = document.createElement('img');
-   mainImg.classList.add('main-img');
-   mainImg.src = '../src/media/pngimg.com - cabbage_PNG8785-min-resize2.png'
-   mainImg.alt = 'An uncooked green cabbage head';
-   const sloganContainer = document.createElement('div');
-   sloganContainer.classList.add('slogan-container');
-   mainDiv.appendChild(mainImg);
-   mainDiv.appendChild(sloganContainer);
-
-   return {
-      mainImg,
-      sloganContainer
-   };
-}
+menuButton.addEventListener('click', () => {
+    removeMainDiv();
+    addMenuContent();
+    console.log('menu');
+})
 
 
-function createSlogan(sloganContainer) {
-   const sloganDiv = document.createElement('div');
-   sloganDiv.classList.add('slogan');
-   
-   const textNodeOne = document.createTextNode('100% CABBAGE,');
-   const textNodeTwo = document.createTextNode('...');
-   const textNodeThree = document.createTextNode('...DAY');
-   
-   const lineBreak = document.createElement('br')
-   
-   const spanElement = document.createElement('span');
-   spanElement.textContent = 'EVERY';
-   
-   sloganDiv.appendChild(textNodeOne);
-   sloganDiv.appendChild(lineBreak);
-   sloganDiv.appendChild(textNodeTwo);
-   sloganDiv.appendChild(spanElement);
-   sloganDiv.appendChild(textNodeThree);
-   
-   sloganContainer.appendChild(sloganDiv);
 
-   return sloganDiv;
-}
-
-function initializePage() {
-   const {header, headerNavContainer} = createHeader();
-   createNav(headerNavContainer);
-   const mainDiv = createMainDiv();
-   const {mainImg, sloganContainer} = createMainDivElements(mainDiv);
-   createSlogan(sloganContainer);
-}
+contactButton.addEventListener('click', () => {
+    removeMainDiv();
+    addContactContent();
+    console.log('contact');
+})
