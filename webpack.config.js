91c89922cd16423ef9886ad0906baf06718de34a
nufc-hub <kmchenouf@gmail.com>
2023-11-16 16:47:05 +0900

    Initial commit

---
+++ webpack.config.js
@@ -0,0 +1,28 @@
+const path = require('path');
+
+module.exports = {
+  entry: './src/index.js',
+  output: {
+    filename: 'main.js',
+    path: path.resolve(__dirname, 'dist'),
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader'],
+      },
+      {
+        test: /\.(png|jpe?g|gif)$/i,
+        use: [
+          {
+            loader: 'file-loader',
+            options: {
+              name: 'images/[name].[ext]', // Output directory and file name pattern
+            },
+          },
+        ],
+      },
+    ],
+  },  
+};
